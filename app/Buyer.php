58a741cf7fc9dd6b5e38285c5e4f37ee4e053bbf
AdalGarcia <adalgarcia17@gmail.com>
2018-02-02 09:50:45 -0600

    Modelos y migraciones necesarios

---
+++ app/Buyer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+/*It is not necessary due to the nature of the relationship*/
+//use Illuminate\Database\Eloquent\Model;
+
+//class Buyer extends Model
+class Buyer extends User
+{
+    //
+}
---
+++ app/Seller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+/*It is not necessary due to the nature of the relationship*/
+//use Illuminate\Database\Eloquent\Model;
+
+//class Seller extends Model
+class Seller extends User
+{
+    //
+}
