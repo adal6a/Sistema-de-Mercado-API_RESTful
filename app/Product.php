ceb569c3ebc5b260f17d7883f0d54b1510d123a6
AdalGarcia <adalgarcia17@gmail.com>
2018-02-02 19:00:28 -0600

    Definicion de propiedades de los modelos y sus scopes

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    const PRODUCTO_DISPONIBLE = '1';
+    const PRODUCTO_NO_DISPONIBLE = '0';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id',
+    ];
+
+    /* Scopes */
+    public function estaDisponible(){
+        return $this->status == Product::PRODUCTO_DISPONIBLE;
+    }
+
+
 }
